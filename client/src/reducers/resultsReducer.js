5a25e9a3b653a2df668d6260c312e04e3846b061
Jeremy Antonoff <jeremyantonoff@gmail.com>
2017-11-09 22:23:28 -0500

    change return to string if payload isnt > 1 to allow no results found

---
+++ client/src/reducers/resultsReducer.js
@@ -0,0 +1,13 @@
+import { GET_RESULTS } from '../actions/types';
+export default (state = [], action) => {
+  switch (action.type) {
+    case GET_RESULTS:
+      if (action.payload.length > 1) {
+        return action.payload;
+      } else {
+        return '';
+      }
+    default:
+      return state;
+  }
+};
