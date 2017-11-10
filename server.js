b741419707e447c837738aba058d92111ac317d6
Jeremy Antonoff <jeremyantonoff@gmail.com>
2017-11-10 03:08:14 -0500

    add body-parser and user routes

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const app = express();
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/User');
 // require('./services/seedToken.js')
 require('./services/passport');
 const authRoutes = require('./routes/authRoutes');
 const apiRoutes = require('./routes/apiRoutes');
+const userApiRoutes = require('./routes/userApiRoutes');
 const PORT = process.env.port || 5000;
 mongoose.connect(keys.mongoURI, { useMongoClient: true });
 
@@ -18,10 +20,13 @@ app.use(
     keys: [keys.cookieKey]
   })
 );
-
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(authRoutes);
 app.use(apiRoutes);
+app.use(userApiRoutes);
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
