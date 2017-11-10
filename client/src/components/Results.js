5fe17d79bffd3f5a7c7acf1f209c3b13893a4149
Jeremy Antonoff <jeremyantonoff@gmail.com>
2017-11-09 22:11:58 -0500

    create class based component to render spotify API results

---
+++ client/src/components/Results.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import './results.css';
+import SearchBar from './SearchBar';
+import * as actions from '../actions/';
+import Card from './Card';
+class Results extends Component {
+  render() {
+    return (
+      <div className="results-page">
+        <div className="top-info">
+          <SearchBar placeholder={'Continue your adventure...'} />
+          <div className="results-info">
+            <ul>
+              <li>If logged in click SAVE to save an item!</li>
+              <li>*Some albums dont have preview audio available</li>
+            </ul>
+          </div>
+        </div>
+        <div className="results-container">
+          <ul>
+            <Card />
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return { results: state.results };
+}
+
+export default connect(mapStateToProps, actions)(Results);
