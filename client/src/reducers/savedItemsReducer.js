8cb809df969bc8cda47f5c21fc0d23e0ba6b1f56
Jeremy Antonoff <jeremyantonoff@gmail.com>
2017-11-11 04:06:12 -0500

    change initial state to be null

--- client/src/reducers/savedItemsReducer.js
+++ client/src/reducers/savedItemsReducer.js
@@ -1,8 +1,8 @@
 import { GET_SAVED_ITEMS } from '../actions/types';
-export default (state = [], action) => {
+export default (state = null, action) => {
   switch (action.type) {
     case GET_SAVED_ITEMS:
-      return action.payload;
+      return action.payload || [];
     default:
       return state;
   }
