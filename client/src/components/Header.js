d3f3d0b1401b6469233bfff0ff1de34efe179524
Jeremy Antonoff <jeremyantonoff@gmail.com>
2017-11-11 04:03:11 -0500

    change header logic to handle initial state load

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,54 +4,59 @@ import './Header.css';
 
 class Header extends Component {
   renderContent() {
-    if (!this.props.auth) {
-      return (
-        <div>
-          <li>
-            <a className="login-button" href="/auth/spotify">
-              <img
-                src="/images/spotify.png"
-                className="loginIMG"
-                alt="Spotify login"
-              />
-              Spotify Login
-            </a>
-          </li>
-          <li>
-            <a className="login-button" href="/auth/google">
-              <img
-                src="/images/google.png"
-                className="loginIMG"
-                alt="Google login"
-              />
-              Google Login
-            </a>
-          </li>
-        </div>
-      );
-    } else {
-      const { googleName } = this.props.auth.google || {};
-      const { spotifyID } = this.props.auth.spotify || {};
-      return (
-        <div>
-          <p className="welcome-msg">
-            Welcome {googleName || spotifyID || 'User!'}!
-          </p>
-          <li />
-          <li>
-            <a className="login-button logged-in" href="/dashboard">
-              Saved Items
-            </a>
-          </li>
-          <li>
-            <a className="login-button logged-in" href="/auth/logout">
-              Logout
-            </a>
-          </li>
-        </div>
-      );
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <div>
+            <li>
+              <a className="login-button" href="/auth/spotify">
+                <img
+                  src="/images/spotify.png"
+                  className="loginIMG"
+                  alt="Spotify login"
+                />
+                Spotify Login
+              </a>
+            </li>
+            <li>
+              <a className="login-button" href="/auth/google">
+                <img
+                  src="/images/google.png"
+                  className="loginIMG"
+                  alt="Google login"
+                />
+                Google Login
+              </a>
+            </li>
+          </div>
+        );
+      default: {
+        const { googleName } = this.props.auth.google || {};
+        const { spotifyID } = this.props.auth.spotify || {};
+        return (
+          <div>
+            <p className="welcome-msg">
+              Welcome {googleName || spotifyID || 'User!'}!
+            </p>
+            <li />
+            <li>
+              <a className="login-button logged-in" href="/dashboard">
+                Saved Items
+              </a>
+            </li>
+            <li>
+              <a className="login-button logged-in" href="/auth/logout">
+                Logout
+              </a>
+            </li>
+          </div>
+        );
+      }
     }
   }
+
   render() {
     return (
       <nav className="top-header">
