d27f70f5d8a3677e1b1834f053e8b41a4f1726db
Jeremy Antonoff <jeremyantonoff@gmail.com>
2017-11-11 04:03:57 -0500

    change logic to handle initial state load and returned empty objects

--- client/src/components/Results.js
+++ client/src/components/Results.js
@@ -6,6 +6,36 @@ import * as actions from '../actions/';
 import Card from './Card';
 class Results extends Component {
   render() {
+    if (!this.props.results) {
+      return (
+        <div className="results-page">
+          <div className="top-info">
+            <SearchBar placeholder={'Continue your adventure...'} />
+            <div className="results-info">
+              <ul>
+                <li>If logged in click SAVE to save an item!</li>
+                <li>*Some albums dont have preview audio available</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      );
+    } else if (this.props.results < 1) {
+      return (
+        <div className="results-page">
+          <div className="top-info">
+            <SearchBar placeholder={'Continue your adventure...'} />
+            <div className="results-info">
+              <ul>
+                <li>If logged in click SAVE to save an item!</li>
+                <li>*Some albums dont have preview audio available</li>
+              </ul>
+              <div className="no-results">No results found!</div>
+            </div>
+          </div>
+        </div>
+      );
+    }
     return (
       <div className="results-page">
         <div className="top-info">
