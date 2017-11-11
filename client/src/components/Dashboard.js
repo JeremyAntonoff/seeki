6e2d396ecd3f15026ee09d782d5ce49924d06bd5
Jeremy Antonoff <jeremyantonoff@gmail.com>
2017-11-11 04:02:32 -0500

    create if statements to handle different state changes

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -2,20 +2,39 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
 import Card from './Card';
-import './card.css';
+import './dashboard.css';
 class Dashboard extends Component {
   componentDidMount() {
     this.props.getSavedItems();
   }
   render() {
+    if (!this.props.savedItems) {
+      return (
+        <div className="dashboard-page">
+          <div className="dashboard-container" />
+        </div>
+      );
+    } else if (this.props.savedItems < 1) {
+      return (
+        <div className="dashboard-page">
+          <div className="dashboard-container">
+            <div className="no-results">No results found!</div>
+          </div>
+        </div>
+      );
+    }
     return (
-      <ul>
-        <Card
-          results={this.props.savedItems}
-          button={'DELETE'}
-          dashboard={true}
-        />
-      </ul>
+      <div className="dashboard-page">
+        <div className="dashboard-container">
+          <ul>
+            <Card
+              results={this.props.savedItems}
+              button={'DELETE'}
+              dashboard={true}
+            />
+          </ul>
+        </div>
+      </div>
     );
   }
 }
