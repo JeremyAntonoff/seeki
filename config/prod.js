0ba94c1bbcc136677387a0d32e0f9683fb0f600c
Jeremy Antonoff <jeremyantonoff@gmail.com>
2017-11-13 00:52:15 -0500

    add comma after value

--- config/prod.js
+++ config/prod.js
@@ -3,8 +3,8 @@ module.exports = {
   cookieKey: process.env.COOKIE_KEY,
   spotifyClientID: process.env.SPOTIFY_CLIENT_ID,
   spotifyClientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-  googleClientID: process.env.GOOGLE_CLIENT_ID
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   spotifyAccessToken: process.env.SPOTIFY_ACCESS_TOKEN,
   spotifyRefreshToken: process.env.SPOTIFY_REFRESH_TOKEN,
   spotifyCallbackURI: process.env.SPOTIFY_CALLBACK_URI
