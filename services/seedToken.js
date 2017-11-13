8678b93b5fb6e77b6c34302155c7c632fcfdd7ff
Jeremy Antonoff <jeremyantonoff@gmail.com>
2017-11-13 00:26:57 -0500

    change location of keys file

--- services/seedToken.js
+++ services/seedToken.js
@@ -1,8 +1,9 @@
 require('../models/Token');
 const mongoose = require('mongoose');
 const Token = mongoose.model('Token');
-const keys = require('../keys');
-
+const keys = require('../config/keys');
+console.log('KEY1', keys.spotifyAccessToken);
+console.log('KEY2', keys.spotifyRefreshToken);
 Token.create({
   name: 'seeki',
   spotifyAccessToken: keys.spotifyAccessToken,
