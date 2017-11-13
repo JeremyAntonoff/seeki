255f069227c06506fce7b843a4b773c017a5e264
Jeremy Antonoff <jeremyantonoff@gmail.com>
2017-11-12 22:02:31 -0500

    add middleware to set redirect back to current page when logging in

--- middleware/index.js
+++ middleware/index.js
@@ -2,8 +2,16 @@ const express = require('express');
 const app = express();
 
 module.exports = {
-  logout: function logout(req, res, next) {
+  logout: (req, res, next) => {
     req.logout();
     next();
+  },
+  setRedirect: (req, res, next) => {
+    req.session.redirectTo = req.headers.referer;
+    next();
+  },
+  successRedirect: (req, res) => {
+    destination = req.session.redirectTo || '/';
+    res.redirect(destination);
   }
 };
