661715e5141a5079ed39127cee6fef9054a3261f
Jeremy Antonoff <jeremyantonoff@gmail.com>
2017-11-13 19:39:32 -0500

    add alert module to results component

--- client/src/components/Results.js
+++ client/src/components/Results.js
@@ -2,10 +2,29 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../actions/';
 import SearchBar from './SearchBar';
+import AlertContainer from 'react-alert';
 import Card from './Card';
 import './results.css';
 
 class Results extends Component {
+  constructor(props) {
+    super(props);
+    this.showAlert = this.showAlert.bind(this);
+    this.alertOptions = {
+      offset: 0,
+      position: 'top right',
+      theme: 'dark',
+      time: 5000,
+      transition: 'scale'
+    };
+  }
+
+  showAlert = alertMessage => {
+    this.msg.success('Item has been saved!', {
+      time: 2500
+    });
+  };
+
   renderTopContent() {
     return (
       <div className="top-info">
@@ -31,18 +50,25 @@ class Results extends Component {
     }
     return (
       <div className="results-page">
-        <div className="top-info">
-          <SearchBar placeholder={'Continue your adventure...'} />
-          <div className="results-info">
+        <AlertContainer ref={a => (this.msg = a)} {...this.alertOptions} />
+        <div className="results-section">
+          <div className="top-info">
+            <SearchBar placeholder={'Continue your adventure...'} />
+            <div className="results-info">
+              <ul>
+                <li>*Some albums dont have preview audio available</li>
+              </ul>
+            </div>
+          </div>
+          <div className="results-container">
             <ul>
-              <li>*Some albums dont have preview audio available</li>
+              <Card
+                results={this.props.results}
+                button={'SAVE THIS SONG'}
+                alert={() => this.showAlert(this.props.alert)}
+              />
             </ul>
           </div>
-        </div>
-        <div className="results-container">
-          <ul>
-            <Card results={this.props.results} button={'SAVE THIS SONG'} />
-          </ul>
         </div>
       </div>
     );
