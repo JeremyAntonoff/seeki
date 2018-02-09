18c99fb26c51dd13db95956cb7c0fbd31fc861db
Jeremy Antonoff <jeremyantonoff@gmail.com>
2018-02-08 23:00:18 -0500

    took out console.log statements

--- services/seedToken.js
+++ services/seedToken.js
@@ -2,8 +2,6 @@ require('../models/Token');
 const mongoose = require('mongoose');
 const Token = mongoose.model('Token');
 const keys = require('../config/keys');
-console.log('KEY1', keys.spotifyAccessToken);
-console.log('KEY2', keys.spotifyRefreshToken);
 Token.create({
   name: 'seeki',
   spotifyAccessToken: keys.spotifyAccessToken,
